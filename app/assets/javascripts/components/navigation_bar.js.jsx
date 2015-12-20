2fa0d9f796372fb5231016ab0706706ef25f6eb5
Talya Stern <talya.stern@klarna.com>
2015-12-20 11:48:55 +0200

    prettifying the navigation bar.

--- app/assets/javascripts/components/navigation_bar.js.jsx
+++ app/assets/javascripts/components/navigation_bar.js.jsx
@@ -1,13 +1,34 @@
 class NavigationBar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-       
-        <div className="navbar-center">
-          <AddSnitch/>
-        </div>
-        <div className="navbar-left logo">SHUSHUSHU</div>
-      </nav>
+      <nav className="navbar navbar-fixed-top" role="navigation">
+        <div className="navbar-collapse collapse">
+        <ul className="nav navbar-nav navbar-left">
+          <li>
+            <a href="#">
+              LOGO
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-pencil-square-o fa-3x"></i>
+            </a>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <a href="#">
+              <i className="fa fa-child fa-3x"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-sign-out fa-3x"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     );
   }
 }
