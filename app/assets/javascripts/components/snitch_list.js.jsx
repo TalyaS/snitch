976a8d2a2adb80b5f9f22a87a7944aaaca10077e
Talya Stern <talya.stern@klarna.com>
2016-01-03 13:15:00 +0200

    wip prettify snitch list

--- app/assets/javascripts/components/snitch_list.js.jsx
+++ app/assets/javascripts/components/snitch_list.js.jsx
@@ -2,9 +2,7 @@ class SnitchList extends React.Component {
   render() {
     return (
         <article className="snitch-list" >
-          <ul>
-            { this.props.snitches.map(s => <SnitchView snitch={s} key={s.id} />) }
-          </ul>
+          { this.props.snitches.map(s => <SnitchView snitch={s} key={s.id} />) }
         </article>
       );
   }
--- app/assets/javascripts/components/snitch_view.js.jsx
+++ app/assets/javascripts/components/snitch_view.js.jsx
@@ -1,10 +1,9 @@
 class SnitchView extends React.Component {
   render() {
     return (
-        <li className="snitch-view" key={this.props.key}>
-          <h2 className="snitch-view__body">{this.props.snitch.content}</h2>
-          <p className="snitch-view__body">{this.props.snitch.created_at}</p>
-        </li>
+        <Panel className="snitch-view__body" key={this.props.key}>
+          {this.props.snitch.content}
+        </Panel>
       );
   }
 }
