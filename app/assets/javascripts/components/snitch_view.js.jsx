a285e2772f8222f67497e6f94eb956bbf8fef0ec
Lilach Adir <lilach.adir@klarna.com>
2016-01-10 09:53:51 +0200

    trying to solve new line display bug

--- app/assets/javascripts/components/snitch_view.js.jsx
+++ app/assets/javascripts/components/snitch_view.js.jsx
@@ -2,6 +2,9 @@ class SnitchView extends React.Component {
   render() {
     return (
         <Panel className="snitch-view__body" key={this.props.key}>
+          // line breaks are saved in DB as '\n' and thus are not displayed as new lines. we
+          // couldn't figure out how to include ActionView::Helpers::TextHelper in react and the
+          // next best thing is using a gem called react-simple-format which takes years to install
           {this.props.snitch.content}
         </Panel>
       );
