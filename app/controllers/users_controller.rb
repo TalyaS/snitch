460bedf97c8d43b73a8c3a377acd2b5330ac1149
Talya Stern <talya.stern@klarna.com>
2016-01-17 12:58:19 +0200

    adding event that pops if the user is signed in or not.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     user.google_id_token = params[:google_id_token]
     user.name = params[:name]
     user.image_url = params[:image_url]
+    user.save
 
     render nothing: true, status: 204
   end
