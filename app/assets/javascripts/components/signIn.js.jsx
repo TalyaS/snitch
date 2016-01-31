bb5585ba1cdac82a09417a26d5c331612ce7f77d
Talya Stern <talya.stern@klarna.com>
2016-01-31 13:07:26 +0200

    make the signing button part of the signing component

--- app/assets/javascripts/components/signIn.js.jsx
+++ app/assets/javascripts/components/signIn.js.jsx
@@ -1,12 +1,6 @@
 class SignIn extends React.Component {
   constructor() {
     super();
-
-    Store.listen( data => this.setState(
-        { snitches: data.snitches,
-          showAddSnitch: data.showAddSnitch }
-      )
-    );
   }
 
   render() {
@@ -15,6 +9,7 @@ class SignIn extends React.Component {
         <nav className="navbar navbar-fixed-top" role="navigation"></nav>
         <h1>Welcome to Snitch</h1>
         <h3>Snitch helps you to document your company's most important resource: the things that people say.</h3>
+        <div id="google-signin-button"></div>
       </div>
     );
   }
